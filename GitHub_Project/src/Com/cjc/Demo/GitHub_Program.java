c93df66fd81957233c97917d3ebe075a4f51d80e
jayshree3 <156280181+jayshree3@users.noreply.github.com>
2024-01-12 10:41:45 +0530

    Update GitHub_Program.java
    By Jayshree

--- GitHub_Project/src/Com/cjc/Demo/GitHub_Program.java
+++ GitHub_Project/src/Com/cjc/Demo/GitHub_Program.java
@@ -6,6 +6,11 @@ public class GitHub_Program
 	{
 		System.out.println("Method 1");
 	}
+
+	public void m2()
+	{
+		System.out.println("Method 2");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("Main Srart");
